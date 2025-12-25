45baa9309262850e0f5ba493a72cb14a3026e8e7
Essi Savonius <essi.savonius@hotmail.fi>
2025-12-26 00:58:22 +0200

    4.4

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  likes = 0
+  blogs.forEach((blog) => likes += blog.likes)
+  return likes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
