cb3889265baae981d71acc3f32efaea4cd03d9da
Essi Savonius <essi.savonius@hotmail.fi>
2025-12-26 18:10:05 +0200

    4.8

---
+++ backend/tests/blog_api.test.js
@@ -0,0 +1,53 @@
+const assert = require('node:assert')
+const { test, after, beforeEach } = require('node:test')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    title: 'My first blog',
+    author: "Mikko Rantanen",
+    likes: 5
+  },
+  {
+    title: 'Another blog',
+    author: "Maija Meikäläinen",
+    likes: 0
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+test('a specific blog is within the returned blogs', async () => {
+  const response = await api.get('/api/blogs')
+
+  const titles = response.body.map(e => e.title)
+  assert(titles.includes('My first blog'))
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
