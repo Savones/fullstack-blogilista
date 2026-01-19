e7fb07d3ffefeb6f2b0579b852595651df8d407b
Essi Savonius <essi.savonius@hotmail.fi>
2026-01-19 15:39:05 +0200

    4.17

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -1,14 +1,20 @@
 const blogsRouter = require('express').Router()
 const { resource } = require('../app')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
+  const user = await User.findById(request.body.userId)
+
+  if (!user) {
+    return response.status(400).json({ error: 'userId missing or not valid' })
+  }
 
   if (request.body.title === undefined || request.body.url === undefined) {
     return response.status(400).end()
@@ -18,7 +24,12 @@ blogsRouter.post('/', async (request, response) => {
     blog.likes = 0
   }
 
+  blog.user = user._id
+
   const result = await blog.save()
+  user.blogs = user.blogs.concat(result._id)
+  await user.save()
+
   response.status(201).json(result)
 })
 
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
+const { populate } = require('dotenv')
 
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
@@ -38,7 +39,8 @@ usersRouter.post('/', async (request, response) => {
 })
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
+  const users = await User.find({}).populate('blogs', { title: 1, author: 1, url: 1 })
+
   response.json(users)
 })
 
