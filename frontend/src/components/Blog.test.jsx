9d66e835c475bb714ff4ae3cf8888d19a6b86cd3
Essi Savonius <essi.savonius@hotmail.fi>
2026-01-21 18:06:21 +0200

    5.14

--- frontend/src/components/Blog.test.jsx
+++ frontend/src/components/Blog.test.jsx
@@ -3,17 +3,18 @@ import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 import { expect } from 'vitest'
 
-test('renders title', () => {
-  const blog = {
-    title: 'Test blog',
-    author: 'Test Author',
-    likes: 2,
-    url: 'https://testurl.com',
-    user: {
-      name: 'Test Name',
-      username: 'testUsername'
-    }
+const blog = {
+  title: 'Test blog',
+  author: 'Test Author',
+  likes: 2,
+  url: 'https://testurl.com',
+  user: {
+    name: 'Test Name',
+    username: 'testUsername'
   }
+}
+
+test('renders title', () => {
 
   render(<Blog blog={blog} />)
 
@@ -23,16 +24,6 @@ test('renders title', () => {
 })
 
 test('renders author', () => {
-  const blog = {
-    title: 'Test blog',
-    author: 'Test Author',
-    likes: 2,
-    url: 'https://testurl.com',
-    user: {
-      name: 'Test Name',
-      username: 'testUsername'
-    }
-  }
 
   render(<Blog blog={blog} />)
 
@@ -40,16 +31,6 @@ test('renders author', () => {
 })
 
 test('does not render likes', () => {
-  const blog = {
-    title: 'Test blog',
-    author: 'Test Author',
-    likes: 2,
-    url: 'https://testurl.com',
-    user: {
-      name: 'Test Name',
-      username: 'testUsername'
-    }
-  }
 
   render(<Blog blog={blog} />)
 
@@ -57,18 +38,51 @@ test('does not render likes', () => {
 })
 
 test('does not render url', () => {
-  const blog = {
-    title: 'Test blog',
-    author: 'Test Author',
-    likes: 2,
-    url: 'https://testurl.com',
-    user: {
-      name: 'Test Name',
-      username: 'testUsername'
-    }
-  }
 
   render(<Blog blog={blog} />)
 
   expect(screen.getByText('https://testurl.com'), { exact: false }).not.toBeVisible()
+})
+
+test('does not render url', () => {
+
+  render(<Blog blog={blog} />)
+
+  expect(screen.getByText('https://testurl.com'), { exact: false }).not.toBeVisible()
+})
+
+test('render url after clicking view', async () => {
+
+  render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const element = screen.getByText('https://testurl.com')
+  expect(element).toBeVisible()
+})
+
+test('render likes after clicking view', async () => {
+
+  render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const element = screen.getByText('likes: 2')
+  expect(element).toBeVisible()
+})
+
+test('render name after clicking view', async () => {
+
+  render(<Blog blog={blog} />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const element = screen.getByText('Test Name')
+  expect(element).toBeVisible()
 })
