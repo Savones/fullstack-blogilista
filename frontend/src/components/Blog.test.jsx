d1f262c686c4da196d69dc86a7189910fcd428e3
Essi Savonius <essi.savonius@hotmail.fi>
2026-01-21 18:23:52 +0200

    5.15

--- frontend/src/components/Blog.test.jsx
+++ frontend/src/components/Blog.test.jsx
@@ -15,44 +15,31 @@ const blog = {
 }
 
 test('renders title', () => {
-
   render(<Blog blog={blog} />)
-
-  screen.debug()
-
   const element = screen.findByText('Test blog')
 })
 
 test('renders author', () => {
-
   render(<Blog blog={blog} />)
-
   const element = screen.findByText('Test Author')
 })
 
 test('does not render likes', () => {
-
   render(<Blog blog={blog} />)
-
   expect(screen.getByText('likes: 2'), { exact: false }).not.toBeVisible()
 })
 
 test('does not render url', () => {
-
   render(<Blog blog={blog} />)
-
   expect(screen.getByText('https://testurl.com'), { exact: false }).not.toBeVisible()
 })
 
 test('does not render url', () => {
-
   render(<Blog blog={blog} />)
-
   expect(screen.getByText('https://testurl.com'), { exact: false }).not.toBeVisible()
 })
 
 test('render url after clicking view', async () => {
-
   render(<Blog blog={blog} />)
 
   const user = userEvent.setup()
@@ -64,7 +51,6 @@ test('render url after clicking view', async () => {
 })
 
 test('render likes after clicking view', async () => {
-
   render(<Blog blog={blog} />)
 
   const user = userEvent.setup()
@@ -76,7 +62,6 @@ test('render likes after clicking view', async () => {
 })
 
 test('render name after clicking view', async () => {
-
   render(<Blog blog={blog} />)
 
   const user = userEvent.setup()
@@ -85,4 +70,16 @@ test('render name after clicking view', async () => {
 
   const element = screen.getByText('Test Name')
   expect(element).toBeVisible()
+})
+
+test('call handler twice after clicking like twice', async () => {
+  const mockUpdateBlog = vi.fn()
+  render(<Blog blog={blog} updateBlog={mockUpdateBlog} />)
+
+  const user = userEvent.setup()
+  const likeButton = screen.getByText('like')
+  await user.click(likeButton)
+  await user.click(likeButton)
+
+  expect(mockUpdateBlog.mock.calls).toHaveLength(2)
 })
