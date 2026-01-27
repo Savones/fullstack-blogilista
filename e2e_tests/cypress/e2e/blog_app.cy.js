8ed61d4cc28f5fcc847c81f1e92232d5eb90bfc7
Essi Savonius <essi.savonius@hotmail.fi>
2026-01-27 18:36:28 +0200

    5.17

--- e2e_tests/cypress/e2e/blog_app.cy.js
+++ e2e_tests/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,18 @@
 describe('Blog app', function () {
-  it('front page can be opened', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
     cy.visit('http://localhost:5173')
+  })
+
+  it('Login form is shown', function () {
     cy.contains('Log in')
   })
+  /*
+  it('user can login', function () {
+    cy.contains('label', 'username').type('essuska')
+    cy.contains('label', 'password').type('salainen')
+    cy.contains('button', 'login').click()
+    cy.contains('Minä logged in')
+  })
+  */
 })
