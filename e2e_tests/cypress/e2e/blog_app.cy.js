3079d7deffb74fdd5f0110123349ff19fcbbc78a
Essi Savonius <essi.savonius@hotmail.fi>
2026-01-27 23:44:02 +0200

    5.19

--- e2e_tests/cypress/e2e/blog_app.cy.js
+++ e2e_tests/cypress/e2e/blog_app.cy.js
@@ -24,9 +24,31 @@ describe('Blog app', function () {
 
     it('fails with wrong credentials', function () {
       cy.contains('label', 'username').type('essusk')
+      cy.contains('label', 'password').type('salainen')
+      cy.contains('button', 'login').click()
+      cy.contains('wrong username or password')
+
+      cy.contains('label', 'username').type('essuska')
       cy.contains('label', 'password').type('salaine')
       cy.contains('button', 'login').click()
       cy.contains('wrong username or password')
+    })
+
+    describe('When logged in', function () {
+      beforeEach(function () {
+        cy.login({ username: 'essuska', password: 'salainen' })
+      })
+
+      it('A blog can be created', function () {
+        cy.contains('new blog').click()
+        cy.contains('label', 'title').type('New blog title')
+        cy.contains('label', 'author').type('New Author')
+        cy.contains('label', 'url').type('newblog.fi')
+        cy.get('button').contains('create').click()
+
+        cy.contains('blog New blog title by author New Author was successfully added.')
+        cy.get('.blog').contains('New blog title')
+      })
     })
   })
 })
